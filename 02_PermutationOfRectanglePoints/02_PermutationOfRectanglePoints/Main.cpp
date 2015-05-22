55424cf18647298db5ad346105083282bf32c8f1
Subodh Shakya <skya.subodh@gmail.com>
2015-05-21 19:01:42 -0500

    addition of project to find permutation of points in rectangle

---
+++ 02_PermutationOfRectanglePoints/02_PermutationOfRectanglePoints/Main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
+
+// This function will display all permutation of 3 points of rectangle for given area.
+void FindPermutation(int x, int y, float area)
+{
+
+}
