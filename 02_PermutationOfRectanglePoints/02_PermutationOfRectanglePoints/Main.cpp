7fa7f8d45edccacd44ccb63ad02a21fba1a9009e
Subodh Shakya <skya.subodh@gmail.com>
2015-05-21 20:36:20 -0500

    implementation of solution to print combination of points of rectangle

--- 02_PermutationOfRectanglePoints/02_PermutationOfRectanglePoints/Main.cpp
+++ 02_PermutationOfRectanglePoints/02_PermutationOfRectanglePoints/Main.cpp
@@ -2,13 +2,57 @@
 
 using namespace std;
 
+void PrintCombination(int x, int y, int area);
+void PrintAllPoints(int x, int y, int l, int b);
+
 int main()
 {
-
+	int x, y;	
+	int area = 0;
+	cout << "Enter x coordinate: ";
+	cin >> x;
+	cout << "\nEnter y coordinate: ";
+	cin >> y;
+	cout << "\nEnter area: ";
+	cin >> area;
+	PrintCombination(x, y, area);
 }
 
-// This function will display all permutation of 3 points of rectangle for given area.
-void FindPermutation(int x, int y, float area)
+void PrintCombination(int x, int y, int area)
 {
+	for (int l = 1; l <= area; l++)
+	{
+		for (int b = 1; b <= area; b++)
+		{
+			if (area == (l*b))
+			{
+				PrintAllPoints(x, y, l, b);
+			}
+		}
+	}
+}
 
+void PrintAllPoints(int x, int y, int l, int b)
+{
+	int px1 = x; int py1 = y;
+	int px2 = x + l; int py2 = y;
+	int px3 = x + l; int py3 = y + b;
+	int px4 = x; int py4 = y + b;
+	cout << "(" << px2 << "," << py2 << ")" << "," << "(" << px3 << "," << py3 << ")" << "," << "(" << px4 << "," << py4 << ")" << endl;
+	//reflected vertically
+	px2 = x - l; py2 = y;
+	px3 = x - l; py3 = y + b;
+	cout << "(" << px2 << "," << py2 << ")" << "," << "(" << px3 << "," << py3 << ")" << "," << "(" << px4 << "," << py4 << ")" << endl;
+	
+	//reflected horizontally
+	px2 = x + l; py2 = y;
+	px3 = x + l; py3 = y - b;
+	px4 = x; py4 = y - b;
+	cout << "(" << px2 << "," << py2 << ")" << "," << "(" << px3 << "," << py3 << ")" << "," << "(" << px4 << "," << py4 << ")" << endl;
+
+	//reflected in x = -y
+	px2 = x - l; py2 = y;
+	px3 = x - l; py3 = y - b;
+	px4 = x; py4 = y - b;
+	cout << "(" << px2 << "," << py2 << ")" << "," << "(" << px3 << "," << py3 << ")" << "," << "(" << px4 << "," << py4 << ")" << endl;
 }
